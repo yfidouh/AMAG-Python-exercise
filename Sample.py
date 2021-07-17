6acef6edae1bf289698fedba46f3f65d655fcaf0
Yahya Fidouh <yfidouh@gmail.com>
2021-07-17 17:26:46 +1000

    first commit

---
+++ Sample.py
@@ -0,0 +1,20 @@
+x = 1
+y = 2
+z = "addition"
+
+if z == "addition":
+    print(x+y)
+if z == "multiplication":
+    print(x*y)
+if z == "exponentiation":    
+    print(x**y)
+if z == "subtraction":
+    print(x-y)
+if z == "division":
+    if y is 0:
+        print("Error")
+    print(x/y)
+
+
+
+
